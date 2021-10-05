b0d47524da54c6636aa7db80207733ca2fdf71ad
peng gao <51525797+gaopengcuhk@users.noreply.github.com>
2021-10-05 22:18:56 +0800

    Update transformer.py

--- models/transformer.py
+++ models/transformer.py
@@ -17,40 +17,6 @@ import pdb
 import torch.nn as nn
 from itertools import repeat
 
-class SpatialDropout(nn.Module):
-    """
-    空间dropout，即在指定轴方向上进行dropout，常用于Embedding层和CNN层后
-    如对于(batch, timesteps, embedding)的输入，若沿着axis=1则可对embedding的若干channel进行整体dropout
-    若沿着axis=2则可对某些token进行整体dropout
-    """
-    def __init__(self, drop=0.5):
-        super(SpatialDropout, self).__init__()
-        self.drop = drop
-        
-    def forward(self, inputs, noise_shape=None):
-        """
-        @param: inputs, tensor
-        @param: noise_shape, tuple, 应当与inputs的shape一致，其中值为1的即沿着drop的轴
-        """
-        outputs = inputs.clone()
-        if noise_shape is None:
-            noise_shape = (inputs.shape[0], *repeat(1, inputs.dim()-2), inputs.shape[-1])   # 默认沿着中间所有的shape
-        
-        self.noise_shape = noise_shape
-        if not self.training or self.drop == 0:
-            return inputs
-        else:
-            noises = self._make_noises(inputs)
-            if self.drop == 1:
-                noises.fill_(0.0)
-            else:
-                noises.bernoulli_(1 - self.drop).div_(1 - self.drop)
-            noises = noises.expand_as(inputs)    
-            outputs.mul_(noises)
-            return outputs
-            
-    def _make_noises(self, inputs):
-        return inputs.new().resize_(self.noise_shape)
 
 
 
@@ -106,7 +72,7 @@ class Transformer(nn.Module):
                                           return_intermediate=return_intermediate_dec)
 
         self._reset_parameters()
-        self.token_drop = SpatialDropout(drop=0.5)
+#        self.token_drop = SpatialDropout(drop=0.5)
         self.d_model = d_model
         self.nhead = nhead
 
